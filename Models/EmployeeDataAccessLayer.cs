9ee1bc6bb739c40ee98f876c28afb226e44d084d
rafaelnascimentosilva <rafaelnasc.silv@hotmail.com>
2019-01-30 22:21:56 -0300

    frist commit

---
+++ Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReactCrudDemo.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+        EnterpriseDBContext db = new EnterpriseDBContext();
+
+        public IEnumerable<TblEmployee> GetAllEmployees()
+        {
+            try {
+                return db.TblEmployee.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public int AddEmployee( TblEmployee employee)
+        {
+            try
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return 1;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int UpdateEmployee(TblEmployee employee)
+        {
+            try
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return 1;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public TblEmployee GetEmployeeData(int id)
+        {
+            try
+            {
+                TblEmployee employee = db.TblEmployee.Find(id);
+                return employee;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public int DeleteEmployee(int id)
+        {
+            try
+            {
+                TblEmployee emp = db.TblEmployee.Find(id);
+                db.TblEmployee.Remove(emp);
+                db.SaveChanges();
+                return 1;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<TblCities> GetCities()
+        {
+            List<TblCities> cities = new List<TblCities>();
+            cities = (from CityList in db.TblCities select CityList).ToList();
+
+            return cities;
+            
+        }
+    }
+}
---
+++ Models/EnterpriseDBContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ReactCrudDemo.Models
+{
+    public partial class EnterpriseDBContext : DbContext
+    {
+        public EnterpriseDBContext()
+        {
+        }
+
+        public EnterpriseDBContext(DbContextOptions<EnterpriseDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCities> TblCities { get; set; }
+        public virtual DbSet<TblEmployee> TblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-RAFADEV\\SQLEXPRESS;Initial Catalog=EnterpriseDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+
+            modelBuilder.Entity<TblCities>(entity =>
+            {
+                entity.HasKey(e => e.CityId)
+                    .HasName("PK__tblCitie__F2D21A96022E3981");
+
+                entity.ToTable("tblCities");
+
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.CityName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblEmployee>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId)
+                    .HasName("PK__tblEmplo__7AD04FF14CBF4F48");
+
+                entity.ToTable("tblEmployee");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(6)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
